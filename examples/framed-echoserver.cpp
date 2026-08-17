cc691e456f123f5735f354e4e97d523ae33065e5
Vladimir Linkov <rackotmell@gmail.com>
2026-08-17 11:52:43 +0300

    Add framed echo server example

---
+++ examples/framed-echoserver.cpp
@@ -0,0 +1,22 @@
+#include <miniasyncnetsockets/miniasyncnetsockets.hpp>
+
+#include <iostream>
+
+int main()
+{
+    miniasyncnetsockets::ServerCallbacks callbacks;
+    callbacks.onFrame = [](miniasyncnetsockets::TcpConnection& connection,
+                           miniasyncnetsockets::Frame frame) {
+        connection.sendFrame(frame);
+    };
+
+    miniasyncnetsockets::TcpServer server(
+        mininetsockets::Endpoint::ipv4Any(12345), std::move(callbacks));
+    server.start();
+    std::cout << "framed echo server listening on " << server.localEndpoint().toString() << '\n';
+    std::cout << "press Enter to stop\n";
+
+    std::string line;
+    std::getline(std::cin, line);
+    server.stop();
+}
